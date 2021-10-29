c5a0e8348c26df1e0aada188f625a9e2f1f096a4
airjuice1 <airjuice1@gmail.com>
2021-10-30 00:52:07 +0300

    sublime 4

--- 1.bat
+++ 1.bat
@@ -1,2 +1,2 @@
-xcopy .\*.* "C:\Users\Juice\AppData\Roaming\Sublime Text 3\Packages\User" /R /I /S /Y /EXCLUDE:excludedfileslist.txt
+xcopy .\*.* "C:\Users\Juice\AppData\Roaming\Sublime Text\Packages\User" /R /I /S /Y /EXCLUDE:excludedfileslist.txt
 PAUSE
